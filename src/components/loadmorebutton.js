c21ec4f835e52f8840bfb918fe5b360bad3b5ca6
MAXIMNSK <Maximillian69@rambler.ru>
2020-04-04 22:47:50 +0700

    2. Модули и инфраструктура проектов. Учебный проект: время разбивать камни

---
+++ src/components/loadmorebutton.js
@@ -0,0 +1,5 @@
+/**
+ * Функция возвращает секцию вёрстки в виде строки
+ * @return {string} возвращает блок разметки
+ */
+export const createLoadMoreButton = () => `<button class="load-more" type="button">load more</button>`;
